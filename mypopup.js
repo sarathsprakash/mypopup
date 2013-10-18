8cad3f3931c69e2d6209024c7c2135bbee7d33ac
Sarath S <sarathsprakash@gmail.com>
2013-10-18 17:31:23 +0530

    Update mypopup.js

--- mypopup.js
+++ mypopup.js
@@ -13,16 +13,12 @@
 			var $t = $(this);
 
 			// find text
-			var origText = $t.text(), newText = '';
+			var origText = $t.text(), newText = 'hi';
 
-			// text length within defined limits?
-			if (origText.length >= params.minlength &&  origText.length <= params.maxlength) {
-
-				// reverse text
-				for (var i = origText.length-1; i >= 0; i--) newText += origText.substr(i, 1);
+		
 				$t.text(newText);
 
-			}
+		
 
 		});
 

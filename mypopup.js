7b4d6062390f41c0f7b159b79f82c6449fddfa9d
Sarath S <sarathsprakash@gmail.com>
2013-10-18 17:38:57 +0530

    Update mypopup.js

--- mypopup.js
+++ mypopup.js
@@ -2,20 +2,9 @@
 
 	// jQuery plugin definition
 	$.fn.mypopup = function() {
+		var a=this.text();
 
-	      	
-
-			// find text
-			var origText = this.text(), newText = '';
-
-			// text length within defined limits?
-			if (origText.length >= params.minlength &&  origText.length <= params.maxlength) {
-
-				// reverse text
-				for (var i = origText.length-1; i >= 0; i--) newText += origText.substr(i, 1);
-				this.text(newText);
-
-			}
+	      this.text('hello'+a);
 
 		return this;
 	};

aba29940be039c3a1e5886a7c827a2d3cd99ef06
Sarath S <sarathsprakash@gmail.com>
2013-10-18 19:30:38 +0530

    Update mypopup.js

--- mypopup.js
+++ mypopup.js
@@ -2,8 +2,11 @@
 
 	// jQuery plugin definition
 	$.fn.mypopup = function() {
+		var styleElement = document.createElement("style");
+                  styleElement.type = "text/css";
+                  styleElement.src= "http://"
 	
-
+              this.parents('html').find('head').appendChild(styleElement);
 	       this.css('color','red');
 
 	

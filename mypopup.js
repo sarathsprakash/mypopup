8b6332095195160b9106229a7641325e362684e3
Sarath S <sarathsprakash@gmail.com>
2013-10-18 19:53:25 +0530

    Update mypopup.js

--- mypopup.js
+++ mypopup.js
@@ -4,13 +4,13 @@
 	$.fn.mypopup = function() {
 		var styleElement = document.createElement("link");
                  styleElement.rel = "stylesheet";
-                 styleElement.href="";
+                 styleElement.href="https://rawgithub.com/sarathsprakash/mypopup/master/ui.css";
                  styleElement.type="text/css";
 		this.each(function()
 		{
 		this.addClass('uitext');
 		});
-	   return this.parents('body').siblings('head').append("<data>hi</data>");
+	   return this.parents('body').siblings('head').append(styleElement);
 	       
 
 	

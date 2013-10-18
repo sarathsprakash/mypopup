fea5d311884ca5f68389fa45f4b0f7fcb0ae4235
Sarath S <sarathsprakash@gmail.com>
2013-10-18 17:17:41 +0530

    Update mypopup.js

--- mypopup.js
+++ mypopup.js
@@ -1,32 +1,12 @@
 (function($) {
 
 	// jQuery plugin definition
-	$.fn.mypopup = function(params) {
+	$.fn.mypopup = function() {
 
-		// merge default and user parameters
-		params = $.extend( {minlength: 0, maxlength: 99999}, params);
-
-		// traverse all nodes
-		this.each(function() {
-
-			// express a single node as a jQuery object
-			var $t = $(this);
-
-			// find text
-			var origText = $t.text(), newText = '';
-
-			// text length within defined limits?
-			if (origText.length >= params.minlength &&  origText.length <= params.maxlength) {
-
-				// reverse text
-				for (var i = origText.length-1; i >= 0; i--) newText += origText.substr(i, 1);
-				$t.text(newText);
-
+		this.style.color='red';
 			}
 
-		});
-
-		// allow jQuery chaining
+		
 		return this;
 	};
 
